a0a07aa1001ba32b68ba43da2b97dec13e15c2a0
jinio <jinioh88@gmail.com>
2020-04-24 07:33:28 +0900

    MemberService 추가

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,10 +2,34 @@ package jpabook.jpashop.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    @PersistenceContext // jpa 관리
-    private EntityManager em;
+//    @PersistenceContext // jpa 관리
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member); // 영속성 컨텍스트에 올림. 키가 id 값이 됨.
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    // jpql: from 의 대상이 테이블이 아닌 객체
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name).getResultList();
+    }
 }
---
+++ src/main/java/jpabook/jpashop/servie/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.servie;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // jpa의 모든 변경은 트랜잭션 안에 있어야 한다. javax꺼도 있지만 스프링이 제공하는걸 쓰는게 좋다. 읽기전용에는 readOnly = true 를 주는게 좋다.
+@RequiredArgsConstructor  // final 필드만 주입. AllArg는 final 인붙여도 됨
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional  // readOnly기본이 false라서
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다!");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
