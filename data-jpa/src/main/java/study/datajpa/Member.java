cc2d50b8119a4baeecb1d27c767245b41a41a54c
jinioh88 <jinioh88@gmail.com>
2020-04-28 12:53:55 +0900

    연관관계 테스트

--- data-jpa/src/main/java/study/datajpa/Member.java
+++ data-jpa/src/main/java/study/datajpa/Member.java
@@ -1,23 +1,49 @@
 package study.datajpa;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name", "age"})
 public class Member {
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String name;
 
-    protected Member() {}
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String name) {
+        this(name, 0);
+    }
+
+    public Member(String name, int age) {
+        this(name, age, null);
+    }
+
+    public Member(String name, int age, Team team) {
         this.name = name;
+        this.age = age;
+        
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    private void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
---
+++ data-jpa/src/test/java/study/datajpa/MemberTest.java
@@ -0,0 +1,45 @@
+package study.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@SpringBootTest
+public class MemberTest {
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void 멤버_팀_테스트() {
+        Team team1 = new Team("북산");
+        Team team2 = new Team("능남");
+        em.persist(team1); em.persist(team2);
+
+        Member member1 = new Member("강백호", 17, team1);
+        Member member2 = new Member("서태웅", 17, team1);
+        em.persist(member1); em.persist(member2);
+
+        Member member3 = new Member("변덕규", 19 , team2);
+        Member member4 = new Member("윤대협", 18 , team2);
+        em.persist(member3); em.persist(member4);
+
+        // 초기화
+        em.flush();
+        em.clear();
+
+        List<Member> findMembers =
+                em.createQuery("select m from Member m", Member.class).getResultList();
+
+        for (Member member : findMembers) {
+            System.out.println("member : " + member);
+            System.out.println("member-Team : " + member.getTeam());
+        }
+    }
+}
