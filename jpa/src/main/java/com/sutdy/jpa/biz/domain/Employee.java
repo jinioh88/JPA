2f0ce75d13ef30941fcc0b84634d269a6628de5a
jinioh88 <jinioh88@gmail.com>
2021-02-26 05:29:17 +0900

    GenerationType AUTO

--- jpa/src/main/java/com/sutdy/jpa/biz/domain/Employee.java
+++ jpa/src/main/java/com/sutdy/jpa/biz/domain/Employee.java
@@ -1,19 +1,14 @@
 package com.sutdy.jpa.biz.domain;
 
 import lombok.*;
-
 import javax.persistence.*;
-import java.util.Date;
 
 @Data
 @Entity
 @Table(name = "S_EMP")
-@TableGenerator(name = "SEQ_GENERATOR", table = "SHOPPING_SEQUENCES",
-pkColumnName = "SEQ_NAME", pkColumnValue = "EMP_SEQ", valueColumnName = "NEXT_VALUE",
-initialValue = 0, allocationSize = 1)
 public class Employee {
 
-    @Id @GeneratedValue(strategy = GenerationType.TABLE, generator = "SEQ_GENERATOR")
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
